13830b708c2abeecb1e15d1ec8843ccf647ee74d
raojunbo <raojunbo@babytree-inc.com>
2022-11-17 09:39:55 +0800

    服务端的socket成功

--- socketIO/socketIO_Server.js
+++ socketIO/socketIO_Server.js
@@ -1,7 +1,7 @@
-// const {ChatModel} = require('../db/models')
+
 import {ChatModel} from '../db/models'
 
-module.exports = function (server) {
+export default function (server) {
     const io = require('socket.io')(server)
     io.listen(4001)
 
@@ -15,20 +15,10 @@ module.exports = function (server) {
             const chat_id =  [from, to].sort().join('_')
             const create_time = Date.now()
             new ChatModel({from, to, content, chat_id, create_time}).save(function(err,chatMsg){
-                // 暂时向所有链接的
+                // 暂时向所有链接都发送消息
                 console.log('服务器向到浏览器的消息', { from, to, content })
                 io.emit("receiveMsg",chatMsg)
             })
-
-
-
-            // 向全局发送消息
-            // io.emit('receiveMsg', data.name + '_' +  Date.now())
-
-            // 向某一个发送消息
-            // socket.emit('receiveMsg', data.name + '_' +  Date.now())
-
-            // console.log('服务器向浏览器发送消息', data.name + '_' +  Date.now())
         })
     })
 }
